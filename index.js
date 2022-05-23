86b1e1ac22f00045c81b369dfff96c7d5c74077c
Isabell Paulmann <isabell.paulmann@hotmail.de>
2022-05-23 10:29:11 +0200

    Add JS

--- index.js
+++ index.js
@@ -1,17 +1,96 @@
-let activateButton = document.querySelector('[data-js="clickAnswer"]');
-let answer = document.querySelector('[data-js="answer"]');
+const activateButton = document.querySelector('[data-js="clickAnswer"]');
+const activateButton2 = document.querySelector('[data-js="clickAnswer2"]');
+const activateButton3 = document.querySelector('[data-js="clickAnswer3"]');
+const activateButton4 = document.querySelector('[data-js="clickAnswer4"]');
+const answer = document.querySelector('[data-js="answer"]');
+const answer2 = document.querySelector('[data-js="answer2"]');
+const allBookmarkButtons = document.querySelectorAll('[data-js="bookmark"]');
+const submitButton = document.querySelector('[data-js="submit"]');
 
-activateButton.addEventListener('click', showAnswer);
+//bookmark
+allBookmarkButtons.forEach(bookmark => {
+  bookmark.addEventListener('click', () => {
+    bookmark.classList.toggle('questcard--colorchanges');
+  });
+});
 
-function showAnswer() {
-  answer.classList.toggle('');
-}
+//buttons
+activateButton.addEventListener('click', () => {
+  activateButton.classList.toggle('displayNone');
+  activateButton2.classList.toggle('displayPages');
+  answer.classList.toggle('displayPages');
+});
+activateButton2.addEventListener('click', () => {
+  activateButton2.classList.toggle('displayPages');
+  activateButton.classList.toggle('displayNone');
+  answer.classList.toggle('displayNone');
+});
+activateButton3.addEventListener('click', () => {
+  activateButton3.classList.toggle('displayNone');
+  activateButton4.classList.toggle('displayPages');
+  answer2.classList.toggle('displayPages');
+});
+activateButton4.addEventListener('click', () => {
+  activateButton4.classList.toggle('displayPages');
+  activateButton3.classList.toggle('displayNone');
+  answer2.classList.toggle('displayNone');
+});
 
-const bookmark = document.querySelector('[data-js="bookmark"]');
-bookmark.addEventListener('click', onBookmarkClick);
+///navigation
 
-function onBookmarkClick() {
-  bookmark.classList.toggle('questcard--colorchanges');
-}
+const page1 = document.querySelector('[data-js="home"]');
+const page2 = document.querySelector('[data-js="book"]');
+const page3 = document.querySelector('[data-js="create"]');
+const page4 = document.querySelector('[data-js="profile"]');
+const nav1 = document.querySelector('[data-js="buttonHome"]');
+const nav2 = document.querySelector('[data-js="buttonBook"]');
+const nav3 = document.querySelector('[data-js="buttonCreate"]');
+const nav4 = document.querySelector('[data-js="buttonProfile"]');
 
-//alert("Willkommen in der Quizapp!");
+nav1.addEventListener('click', () => {
+  page1.classList.remove('displayNone');
+  page2.classList.add('displayNone');
+  page3.classList.add('displayNone');
+  page4.classList.add('displayNone');
+  nav1.classList.add('activeButton');
+  nav2.classList.remove('activeButton');
+  nav3.classList.remove('activeButton');
+  nav4.classList.remove('activeButton');
+});
+nav2.addEventListener('click', () => {
+  page1.classList.add('displayNone');
+  page2.classList.remove('pageHome');
+  page3.classList.add('displayNone');
+  page4.classList.add('displayNone');
+  nav1.classList.remove('activeButton');
+  nav2.classList.add('activeButton');
+  nav3.classList.remove('activeButton');
+  nav4.classList.remove('activeButton');
+});
+nav3.addEventListener('click', () => {
+  page1.classList.add('displayNone');
+  page2.classList.add('pageHome');
+  page3.classList.remove('displayNone');
+  page4.classList.add('displayNone');
+  nav1.classList.remove('activeButton');
+  nav2.classList.remove('activeButton');
+  nav3.classList.add('activeButton');
+  nav4.classList.remove('activeButton');
+});
+nav4.addEventListener('click', () => {
+  page1.classList.add('displayNone');
+  page2.classList.add('pageHome');
+  page3.classList.add('displayNone');
+  page4.classList.remove('displayNone');
+  nav1.classList.remove('activeButton');
+  nav2.classList.remove('activeButton');
+  nav3.classList.remove('activeButton');
+  nav4.classList.add('activeButton');
+});
+
+//submitButton
+
+submitButton.addEventListener('click', () => {
+  submitButton.textContent = 'submitted';
+  submitButton.classList.add('submitted');
+});
